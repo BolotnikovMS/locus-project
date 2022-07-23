dc310a388573a47651ea0ea48e47e3a8e7c08f58
BolotnikovMS <bolotnikovms@yandex.ru>
2022-07-23 16:10:57 +0500

    Update

--- js/main.js
+++ js/main.js
@@ -80,10 +80,20 @@ window.addEventListener('DOMContentLoaded', () => {
   const mobNavBtn = document.querySelector('.mobile-nav-button')
   const mobNavIcon = document.querySelector('.mobile-nav-button__icon')
   const mobNav = document.querySelector('.mobile-nav')
+  const mobileNavList = document.querySelector('.mobile-nav-list')
+  const mobileLinks = mobileNavList.querySelectorAll('a[href*="#"]')
 
   mobNavBtn.addEventListener('click', () => {
     mobNavIcon.classList.toggle('active')
     mobNav.classList.toggle('active')
     document.body.classList.toggle('no-scroll')
+  })
+
+  mobileLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      mobNavIcon.classList.toggle('active')
+      mobNav.classList.toggle('active')
+      document.body.classList.toggle('no-scroll')
+    })
   })
 })
